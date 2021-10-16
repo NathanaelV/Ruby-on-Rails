9d85d626d85ac3909c976682ca85bb84514582c5
Nathan <tmntsplinter04@gmail.com>
2021-10-16 20:47:30 -0300

    Alteração no arquio config/routes.rb para a correção do erro.

--- hello/config/routes.rb
+++ hello/config/routes.rb
@@ -1,5 +1,20 @@
-# Rails.application.routes.draw do
-Hello::Application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get ':cotroller(/:action(/:id(.:format)))'
+Rails.application.routes.draw do
+# Hello::Application.routes.draw do
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get ':cotroller(/:action(/:id(.:format)))'
+
+  resources :items
+
+  root 'items#index'
+
+  get 'items/index', to:'items#index'
+  get 'items/show', to:'items#show'
+  get 'items/new', to:'items#new'
+  get 'items/edit', to:'items#edit'
+  post '/items/create', to:'items#create'
+  post '/items/update', to:'items#update'
+  post '/items/destroy', to:'items#destroy'
+  get '/users/userindex', to: 'users#userindex'
+  get '/users/usershow', to: 'users#usershow'
+  get '/users/usernew', to: 'users#usernew'
 end
