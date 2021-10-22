a31704771c19262e8eb5c41cc8cc74a40e4eacfd
NathanaelV <tmntsplinter04@gmail.com>
2021-10-22 20:49:50 -0300

    Criação do link para criar um novo item da lista e opção de retornar ao menu nas páginas de Edição e Criação de itens.

--- task_list/config/routes.rb
+++ task_list/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'home#welcome'
+  root to: 'home#welcome' # root define a rota '/' da aplicação
   resources :tasks
   
 end
