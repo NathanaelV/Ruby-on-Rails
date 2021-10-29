760f03177696f251ca8f827bbf638202f94e44e7
NathanaelV <tmntsplinter04@gmail.com>
2021-10-29 08:46:40 -0300

    Correção de erros e edição habilitada

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -21,11 +21,15 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def update
     @article = Article.find(params[:id])
     
     if @article.update(article_params)
-      redirect_to
+      redirect_to @article
     else
       render :edit
     end
